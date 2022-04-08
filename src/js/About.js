a7c393b89e9635267ed81a573cddad691dba4c0b
Candice Ariel <candicewangsa@gmail.com>
2022-04-08 17:11:29 +0700

    Object movement on keyboard input

--- src/js/About.js
+++ src/js/About.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
+import { OrbitControls } from "../../public/js/OrbitControls.js";
 
 import { OBJLoader } from "../../public/js/obj.js";
+import { MTLLoader } from "../../public/js/mtl.js";
 
 const aboutcontainer = document.getElementsByClassName(
   "about_canvas_container"
@@ -14,76 +16,152 @@ let mouseX = 0, mouseY = 0;
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
-let sillaCoffeeTable, sillaCoffeeTable2;
+let sillaCoffeeTable,sillaCoffeeTable2, bathroomMtl;
 
-//ADD CAMERA
-camera = new THREE.PerspectiveCamera( 1000, window.innerWidth / window.innerHeight, 2, 100 );
-camera.position.set( -5, 50, 5 );
 
-//SCENE
-//create new scene 
-scene = new THREE.Scene();
-scene.background = new THREE.Color("black");
+function init(){
+  //ADD CAMERA
+  camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.x += ( mouseX - camera.position.x ) * .05;
+	camera.position.y += ( - mouseY - camera.position.y ) * .05;
+  //camera.position.set( -2.3,0,8 );
+  console.log(camera.position.x);
 
-//add ambient light
-ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-scene.add( ambientLight );
+  //SCENE
+  //create new scene 
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("black");
 
-//add point light
-pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-camera.add( pointLight );
-scene.add( camera );
+  //add ambient light
+  ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+  scene.add( ambientLight );
 
-//RENDERER
-renderer = new THREE.WebGLRenderer({
-  antialias: true,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setAnimationLoop(animation);
+  //add point light
+  pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+  camera.add( pointLight );
+  scene.add( camera );
 
-//CHECK IF CANVAS CONTAINER RENDERED
-//ADD RENDERE TO CANVAS
-function checkCanvas() {
-  if (aboutcontainer.length > 0) {
-    aboutcontainer[0].appendChild(renderer.domElement);
-  } else {
-    setTimeout(checkCanvas, 1000);
+  //RENDERER
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.setAnimationLoop(animation);
+
+  //CHECK IF CANVAS CONTAINER RENDERED
+  //ADD RENDERE TO CANVAS
+  function checkCanvas() {
+    if (aboutcontainer.length > 0) {
+      aboutcontainer[0].appendChild(renderer.domElement);
+    } else {
+      setTimeout(checkCanvas, 1000);
+    }
   }
-}
 
-// load model
-const objLoader = new OBJLoader()
-function loadModel(){
-  objLoader.load(
-    'model/obj/3d-model.obj',
-    (object) => {
-          // (object.children[0] as THREE.Mesh).material = material
-          // object.traverse(function (child) {
-          //     if ((child as THREE.Mesh).isMesh) {
-          //         (child as THREE.Mesh).material = material
-          //     }
-          // })
-      sillaCoffeeTable = object;
-      scene.add(sillaCoffeeTable);
-      object.position.x = 0;
-      object.position.y = 1.5;
-      object.scale.set( 0.03, 0.03, 0.03 );
-      object.rotateY( Math.PI / 1);
-      object.rotateZ( Math.PI / 1);
-    },
-    (xhr) => {
-      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-      console.log(error)
-    },
-  );
+  //ADD OBJECT 1
+  const mtlLoader = new MTLLoader();
+  function loadMaterial(){
+    mtlLoader	.load( 'model/mtl/SillaCoffeeTable.mtl', function ( materials ) {
+
+      materials.preload();
+      bathroomMtl = materials;
+      loadModel2(bathroomMtl);
+    
+    })
+  }
+
+  // load model
+  const objLoader = new OBJLoader()
+  function loadModel(bathroomMtl){
+    objLoader.setMaterials(bathroomMtl);
+    objLoader.load(
+      'model/obj/SillaCoffeeTable.obj',
+      (object) => {
+            // (object.children[0] as THREE.Mesh).material = material
+            // object.traverse(function (child) {
+            //     if ((child as THREE.Mesh).isMesh) {
+            //         (child as THREE.Mesh).material = material
+            //     }
+            // })
+        sillaCoffeeTable = object;
+        scene.add(sillaCoffeeTable);
+        object.position.x = -5;
+        object.position.y = 0;
+        object.position.z = -10;
+        object.scale.set( 0.04, 0.04, 0.04 );
+        object.rotateY( Math.PI / 1);
+        // object.rotateZ( Math.PI / 1);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+      },
+      (error) => {
+        console.log(error)
+      },
+    );
+  }
+
+  //ADD OBJECT 2
+  const mtlLoader2 = new MTLLoader();
+  function loadMaterial2(){
+    mtlLoader2	.load( 'model/mtl/SillaCoffeeTable.mtl', function ( materials ) {
+
+      materials.preload();
+      bathroomMtl = materials;
+      loadModel(bathroomMtl);
+    
+    })
+  }
+
+  // load model
+  const objLoader2 = new OBJLoader()
+  function loadModel2(bathroomMtl){
+    objLoader2.setMaterials(bathroomMtl);
+    objLoader2.load(
+      'model/obj/SillaCoffeeTable.obj',
+      (object2) => {
+            // (object.children[0] as THREE.Mesh).material = material
+            // object.traverse(function (child) {
+            //     if ((child as THREE.Mesh).isMesh) {
+            //         (child as THREE.Mesh).material = material
+            //     }
+            // })
+        sillaCoffeeTable2 = object2;
+        scene.add(sillaCoffeeTable2);
+        sillaCoffeeTable2.position.x = 5;
+        sillaCoffeeTable2.position.y = 0;
+        sillaCoffeeTable2.position.z = -10;
+        sillaCoffeeTable2.scale.set( 0.04, 0.04, 0.04 );
+        sillaCoffeeTable2.rotateY( Math.PI / 1);
+        sillaCoffeeTable2.rotateY( Math.PI / 1.1);
+        // object.rotateZ( Math.PI / 1);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+      },
+      (error) => {
+        console.log(error)
+      },
+    );
+  }
+
+  const controls = new OrbitControls( camera, renderer.domElement );
+				controls.addEventListener( 'change', render ); // use if there is no animation loop
+				controls.minDistance = 2;
+				controls.maxDistance = 10;
+				controls.target.set( 0, 0, - 0.2 );
+				controls.update();
+
+  //RUN FUNCTIONS
+  checkCanvas();
+  loadMaterial();
+  loadMaterial2();
 }
 
 var render = function() {
   requestAnimationFrame(render);
-  camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y += ( - mouseY - camera.position.y ) * .05;
+  // camera.position.x += ( mouseX - camera.position.x ) * .05;
+	// camera.position.y += ( - mouseY - camera.position.y ) * .05;
 
 	camera.lookAt( scene.position );
   renderer.render(scene, camera);
@@ -105,17 +183,18 @@ function onWindowResize() {
 
 }
 
-function onDocumentMouseMove( event ) {
+function onPointerDown( event ) {
 
   mouseX = ( event.clientX - windowHalfX ) / 2;
   mouseY = ( event.clientY - windowHalfY ) / 2;
+  console.log(sillaCoffeeTable2.position);
 
 }
 
 // run functions
-checkCanvas();
-loadModel();
+init();
 render();
 
 window.addEventListener( 'resize', onWindowResize );
+document.addEventListener( 'pointerdown', onPointerDown );
 //document.addEventListener( 'mousemove', onDocumentMouseMove );
--- src/js/Home.js
+++ src/js/Home.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 
 import { OBJLoader } from "../../public/js/obj.js";
 import { MTLLoader } from "../../public/js/mtl.js";
+import { ConeBufferGeometry } from "three";
 
 
 const homecontainer = document.getElementsByClassName(
@@ -15,97 +16,127 @@ let ambientLight, pointLight;
 let mouseX = 0, mouseY = 0;
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
+let xSpeed = 0.5;
+let zSpeed = 0.5;
+let ySpeed = 0.0001;
 
 let bathroomModel, bathroomMtl;
+let cube;
 
-//ADD CAMERA
-camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-camera.position.set( -5, 10, 5 );
+function init(){
+  //ADD CAMERA
+  camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 500 );
+  camera.position.x += ( mouseX - camera.position.x ) * .05;
+	camera.position.y += ( mouseY - camera.position.x ) * .05;
 
-//SCENE
-//create new scene 
-scene = new THREE.Scene();
-scene.background = new THREE.Color("black");
+  //SCENE
+  //create new scene 
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("lightgray");
 
-//add ambient light
-ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-scene.add( ambientLight );
+  //add ambient light
+  ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+  scene.add( ambientLight );
 
-//add point light
-pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-camera.add( pointLight );
-scene.add( camera );
+  //add point light
+  pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+  camera.add( pointLight );
+  scene.add( camera );
 
-//RENDERER
-renderer = new THREE.WebGLRenderer({
-  antialias: true,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setAnimationLoop(animation);
+  //RENDERER
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.setAnimationLoop(animation);
 
-//CHECK IF CANVAS CONTAINER RENDERED
-//ADD RENDERE TO CANVAS
-function checkCanvas() {
-  if (homecontainer.length > 0) {
-    homecontainer[0].appendChild(renderer.domElement);
-  } else {
-    setTimeout(checkCanvas, 1000);
-  }
-}
-
-const onProgress = function ( xhr ) {
-
-  if ( xhr.lengthComputable ) {
-
-    const percentComplete = xhr.loaded / xhr.total * 100;
-    console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
-
+  //CHECK IF CANVAS CONTAINER RENDERED
+  //ADD RENDERE TO CANVAS
+  function checkCanvas() {
+    if (homecontainer.length > 0) {
+      homecontainer[0].appendChild(renderer.domElement);
+    } else {
+      setTimeout(checkCanvas, 1000);
+    }
   }
 
-};
+  const onProgress = function ( xhr ) {
 
-const mtlLoader = new MTLLoader();
-function loadMaterial(){
-  mtlLoader	.load( 'model/mtl/3d-model.mtl', function ( materials ) {
+    if ( xhr.lengthComputable ) {
 
-    materials.preload();
-    bathroomMtl = materials;
-    loadModel(bathroomMtl);
-  
-  })
+      const percentComplete = xhr.loaded / xhr.total * 100;
+      console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+
+    }
+
+  };
+
+  const mtlLoader = new MTLLoader();
+  function loadMaterial(){
+    mtlLoader	.load( 'model/mtl/3d-model.mtl', function ( materials ) {
+
+      materials.preload();
+      bathroomMtl = materials;
+      loadModel(bathroomMtl);
+    
+    })
+  }
+
+  // load model
+  // X cord : left and right
+  const objLoader = new OBJLoader()
+  function loadModel(bathroomMtl){
+    objLoader.setMaterials(bathroomMtl);
+    objLoader.load(
+      'model/obj/3d-model.obj',
+      (object) => {
+            // (object.children[0] as THREE.Mesh).material = material
+            // object.traverse(function (child) {
+            //     if ((child as THREE.Mesh).isMesh) {
+            //         (child as THREE.Mesh).material = material
+            //     }
+            // })
+        bathroomModel = object;
+        scene.add(bathroomModel);
+        object.position.x = -0.015;
+        object.position.y = -2;
+        object.position.z = -5;
+        object.scale.set( 0.04, 0.04, 0.04 );
+        object.rotateY( Math.PI / 1);
+      },
+      onProgress,
+      (error) => {
+        console.log(error)
+      },
+    );
+  }
+
+  function addCube(){
+    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    // camera.add(cube);
+    // cube.position.set (0, -1, -1.5);
+
+    cube.position.x = -0.015;
+    cube.position.y = -1;
+    cube.position.z = -2;
+  }
+
+  document.addEventListener("keydown", onDocumentKeyDown);
+
+  checkCanvas();
+  loadMaterial();
+  addCube();
+  render();
 }
 
-// load model
-const objLoader = new OBJLoader()
-function loadModel(bathroomMtl){
-  objLoader.setMaterials(bathroomMtl);
-  objLoader.load(
-    'model/obj/3d-model.obj',
-    (object) => {
-          // (object.children[0] as THREE.Mesh).material = material
-          // object.traverse(function (child) {
-          //     if ((child as THREE.Mesh).isMesh) {
-          //         (child as THREE.Mesh).material = material
-          //     }
-          // })
-      bathroomModel = object;
-      scene.add(bathroomModel);
-      object.position.x = 0;
-      object.position.y = -1.5;
-      object.scale.set( 0.04, 0.04, 0.04 );
-      object.rotateY( Math.PI / 1);
-    },
-    onProgress,
-    (error) => {
-      console.log(error)
-    },
-  );
-}
-
+//
 var render = function() {
   requestAnimationFrame(render);
-  camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y += ( - mouseY - camera.position.y ) * .05;
+  // camera.position.x += ( mouseX - camera.position.x ) * .05;
+	// camera.position.y += ( - mouseY - camera.position.y ) * .05;
 
 	camera.lookAt( scene.position );
   renderer.render(scene, camera);
@@ -123,21 +154,28 @@ function onWindowResize() {
   camera.aspect = canvasWidth / canvasHeight;
   camera.updateProjectionMatrix();
 
-  render();
-
 }
 
-function onDocumentMouseMove( event ) {
+function onDocumentKeyDown( event ) {
 
-  mouseX = ( event.clientX - windowHalfX ) / 2;
+  var keyCode = event.which;
+    if (keyCode == 87) {
+        cube.position.z -= zSpeed;
+    } else if (keyCode == 83) {
+        cube.position.z += zSpeed ;
+    } else if (keyCode == 65) {
+        cube.position.x -= xSpeed;
+    } else if (keyCode == 68) {
+        cube.position.x += xSpeed;
+    } else if (keyCode == 32) {
+        cube.position.set(0, 0, 0);
+    }mouseX = ( event.clientX - windowHalfX ) / 2;
   mouseY = ( event.clientY - windowHalfY ) / 2;
-
+  console.log(cube.position);
 }
 
 // run functions
-checkCanvas();
-loadMaterial();
+init();
 render();
 
-window.addEventListener( 'resize', onWindowResize );
-//document.addEventListener( 'mousemove', onDocumentMouseMove );
+window.addEventListener( "resize", onWindowResize );
